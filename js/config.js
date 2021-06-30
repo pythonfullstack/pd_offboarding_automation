0757d2f11a7dc54471227a2cb05c762cbbaeed17
pythonfullstack <chenfang199531@gmail.com>
2021-07-01 05:27:44 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
     redirectUrl: `https://pythonfullstack.github.io/pd_offboarding_automation/`,
-    clientId: `37e14ac7da1930c2612f9b41ee69eea40087e317f020eb2b9336f4843c59927e`
+    clientId: `64485676ba532e9618d1c3838506ab51645b0b1a9fdfabe5babea0ac2e5c522d`
 };
