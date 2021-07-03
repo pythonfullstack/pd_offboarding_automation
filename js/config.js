43912cd99cedb3b9ea69303ba7fd3756af3151ea
pythonfullstack <chenfang199531@gmail.com>
2021-07-03 08:27:37 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 const APP_CONFIG = {
-    redirectUrl: `https://pythonfullstack.github.io/pd_offboarding_automation/`,
+    // redirectUrl: `https://pythonfullstack.github.io/pd_offboarding_automation/`,
+    redirectUrl: `http://localhost:63344/pd_offboarding_automation/index.html?_ijt=1fossdh4fpsgj7hc4snv8p1pj`,
     clientId: `64485676ba532e9618d1c3838506ab51645b0b1a9fdfabe5babea0ac2e5c522d`
 };
---
+++ js/pagerduty-bulk-offboarding.js
@@ -0,0 +1,6 @@
+const navMap = {
+    "users-export-button": "users-export",
+    "users-import-button": "users-import",
+    "users-edit-button": "users-edit",
+    "auth-button": "auth"
+};
